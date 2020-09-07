520e0635315e0d1e0f269124d8797e0193e852bf
worldjones <worldjones@gmail.com>
2020-09-07 10:58:26 +0200

    test
    kekw

---
+++ src/test/java/dk/cphbusiness/sem3/ci/MyFunctionalityTest.java
@@ -0,0 +1,18 @@
+package dk.cphbusiness.sem3.ci;
+
+import dk.cphbusiness.sem3.ci.MyFunctionality;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author jens
+ */
+public class MyFunctionalityTest {
+
+  @Test
+  public void testFunctionality() {
+    assertEquals(10, MyFunctionality.giveMeTen());
+  }
+
+}
